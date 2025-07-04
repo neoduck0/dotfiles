b156262d1df2b2b89d8c6aad995c97b7b7264f48
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-07-04 22:27:25 +0300

    init

---
+++ content/home/config/nvim/lua/options.lua
@@ -0,0 +1,16 @@
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.softtabstop = 4
+
+vim.o.scrolloff = 10
+
+vim.o.clipboard = "unnamedplus"
+
+vim.o.mouse = ""
+
+vim.wo.number = true
+vim.wo.relativenumber = true
---
+++ content/home/config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        explorer = { enabled = true },
+        picker = {
+            enabled = true,
+            layout = {
+                layout = { backdrop = false }
+            }
+        }
+    }
+}
