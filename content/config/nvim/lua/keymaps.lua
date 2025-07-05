812e0430feb62981466c2c7769ce2b55972093d5
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-07-05 18:00:29 +0300

    feat(neovim): center using zz when using <C-d|u>

--- content/config/nvim/lua/keymaps.lua
+++ content/config/nvim/lua/keymaps.lua
@@ -2,6 +2,8 @@ vim.keymap.set("n", "<leader>e<CR>", vim.cmd.Ex)
 
 vim.keymap.set("n", "j", "jzz")
 vim.keymap.set("n", "k", "kzz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
 vim.keymap.set("n", "<leader>fl", require("telescope.builtin").live_grep)
