722732ec36754882250898baf4f59b0ef89ac354
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-07-08 18:14:46 +0300

    refactor(neovim): revert find files keymap

--- content/config/nvim/lua/keymaps.lua
+++ content/config/nvim/lua/keymaps.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "k", "kzz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("n", "<C-p>", require("telescope.builtin").find_files)
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
 vim.keymap.set("n", "<leader>fl", require("telescope.builtin").live_grep)
 vim.keymap.set("n", "<leader>fw", require("telescope.builtin").buffers)
 vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
