5fe26675531c8ecbbe216798d997981d2b802042
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-07-20 23:56:27 +0300

    neovim: update keymaps

--- content/config/nvim/lua/keymaps.lua
+++ content/config/nvim/lua/keymaps.lua
@@ -1,18 +1,8 @@
-vim.keymap.set("n", "<leader>e", function() Snacks.explorer() end, {silent = true })
-
-vim.keymap.set("n", "j", "jzz", { silent = true })
-vim.keymap.set("n", "k", "kzz", { silent = true })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
-vim.keymap.set("n", "n", "nzz", { silent = true })
-vim.keymap.set("n", "N", "Nzz", { silent = true })
-vim.keymap.set("n", "G", "Gzz", { silent = true })
-
-vim.keymap.set("n", "<leader>bd", "bd", { silent = true })
-
 vim.keymap.set("n", "<leader>ww", "<C-w>w", { silent = true })
 vim.keymap.set("n", "<leader>wv", "<C-w>v", { silent = true })
 vim.keymap.set("n", "<leader>wq", "<C-w>q", { silent = true })
+
+vim.keymap.set("n", "<leader>e", function() Snacks.explorer() end, {silent = true })
 
 vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end, { silent = true })
 vim.keymap.set("n", "<leader>f/", function() Snacks.picker.grep() end, { silent = true })
@@ -24,3 +14,11 @@ vim.keymap.set("n", "<leader>sd", function() Snacks.picker.diagnostics_buffer() 
 vim.keymap.set("n", "<leader>sD", function() Snacks.picker.diagnostics() end, { silent = true })
 
 vim.keymap.set("n", "<leader>t", function() Snacks.terminal() end, { silent = true })
+
+vim.keymap.set("n", "j", "jzz", { silent = true })
+vim.keymap.set("n", "k", "kzz", { silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+vim.keymap.set("n", "n", "nzz", { silent = true })
+vim.keymap.set("n", "N", "Nzz", { silent = true })
+vim.keymap.set("n", "G", "Gzz", { silent = true })
