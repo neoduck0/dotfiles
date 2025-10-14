83b1f6591dd003c95e5e1ea1ca57d66c18f52f3a
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2025-10-14 13:40:30 +0800

    neovim: add support to django template language

--- content/home/config/nvim/lua/plugins/treesitter.lua
+++ content/home/config/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup{
-            ensure_installed = {"bash", "cpp", "css", "go", "html", "javascript", "lua", "python"},
+            ensure_installed = {"bash", "cpp", "css", "go", "html", "htmldjango", "javascript", "lua", "python"},
             auto_install = false,
             highlight = { enable = true }
         }
