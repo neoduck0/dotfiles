65ddb0de59e902ef2a744210b982a23caf1ca035
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-02-14 14:21:06 +0800

    alter(neovim): keymaps

--- content/home/config/nvim/lua/keymaps.lua
+++ content/home/config/nvim/lua/keymaps.lua
@@ -1,29 +1,17 @@
-vim.keymap.set("n", "<leader>ww", "<C-w>w")
-vim.keymap.set("n", "<leader>wq", "<C-w>q")
-vim.keymap.set("n", "<leader>wv", "<C-w>v")
-vim.keymap.set("n", "<leader>wh", "<C-w>s")
-
 vim.keymap.set("n", "<leader>lu", ":Lazy update<CR>")
-vim.keymap.set("n", "<leader>li", ":Lazy install<CR>")
-vim.keymap.set("n", "<leader>lx", ":Lazy clean<CR>")
-
-vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end)
 
 vim.keymap.set("n", "<leader>ex", function() Snacks.explorer() end)
 
-vim.keymap.set("n", "<leader>ow", function()
-    vim.opt.wrap = true
-    vim.opt.linebreak = true
+vim.keymap.set("n", "<leader>wl", function()
+    vim.opt.wrap = not vim.opt.wrap:get()
+    vim.opt.linebreak = vim.opt.wrap:get()
 end)
 
-vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end)
 vim.keymap.set("n", "<leader>f/", function() Snacks.picker.grep() end)
+vim.keymap.set("n", "<leader>fD", function() Snacks.picker.diagnostics() end)
+vim.keymap.set("n", "<leader>fb", function() Snacks.picker.buffers() end)
+vim.keymap.set("n", "<leader>fd", function() Snacks.picker.diagnostics_buffer() end)
+vim.keymap.set("n", "<leader>ff", function() Snacks.picker.files() end)
 vim.keymap.set("n", "<leader>fh", function() Snacks.picker.help() end)
 vim.keymap.set("n", "<leader>fp", function() Snacks.picker.projects() end)
-
-vim.keymap.set("n", "<leader>st", function() Snacks.picker.todo_comments() end)
-vim.keymap.set("n", "<leader>sb", function() Snacks.picker.buffers() end)
-vim.keymap.set("n", "<leader>sd", function() Snacks.picker.diagnostics_buffer() end)
-vim.keymap.set("n", "<leader>sD", function() Snacks.picker.diagnostics() end)
-
-vim.keymap.set("n", "<leader>t", function() Snacks.terminal() end)
+vim.keymap.set("n", "<leader>ft", function() Snacks.picker.todo_comments() end)
