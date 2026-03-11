1d2a5d06ec3bfa4274d4585ece522cc6c99f5d07
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-03-11 20:50:10 +0800

    style(neovim): conform.lua

--- content/config/nvim/lua/plugins/conform.lua
+++ content/config/nvim/lua/plugins/conform.lua
@@ -1,24 +1,24 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				["*"] = { "trim_whitespace", "trim_newlines" },
-				c = { "clang-format" },
-				go = { "gofmt" },
-				json = { "jq" },
-				lua = { lsp_format = "first" },
-				python = { "ruff" },
-				sh = { "shfmt" },
-			},
-			formatters = {
-				["clang-format"] = {
-					prepend_args = { "-style={BasedOnStyle: LLVM, IndentWidth: 4}" },
-				},
-			},
-			format_on_save = {
-				timeout_ms = 500,
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                ["*"] = { "trim_whitespace", "trim_newlines" },
+                c = { "clang-format" },
+                go = { "gofmt" },
+                json = { "jq" },
+                lua = { lsp_format = "first" },
+                python = { "ruff" },
+                sh = { "shfmt" },
+            },
+            formatters = {
+                ["clang-format"] = {
+                    prepend_args = { "-style={BasedOnStyle: LLVM, IndentWidth: 4}" },
+                },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+            },
+        })
+    end,
 }
