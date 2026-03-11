3921d81ecc31e64e4b63c1de55eb15dc7e6dfb6f
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-03-11 21:01:23 +0800

    refactor(neovim): standardize toggle keymaps

--- content/config/nvim/lua/keymaps.lua
+++ content/config/nvim/lua/keymaps.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>e", function() Snacks.explorer() end)
 
-vim.keymap.set("n", "<leader>w", function()
+vim.keymap.set("n", "<leader>tw", function()
     vim.opt.wrap = not vim.opt.wrap:get()
     vim.opt.linebreak = vim.opt.wrap:get()
 end)
