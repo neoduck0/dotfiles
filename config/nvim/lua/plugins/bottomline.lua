f884f52e28c28b5d1c474f09bd82326f0c52c88e
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-08-07 18:05:06 +0800

    nvim: configure lualine sections

--- config/nvim/lua/plugins/bottomline.lua
+++ config/nvim/lua/plugins/bottomline.lua
@@ -1,5 +1,19 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = true,
+	opts = {
+		options = {
+			component_separators = "",
+			section_separators = "",
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+
+			lualine_x = { "lsp_status", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+	},
 }
