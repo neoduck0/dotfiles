f69518a4416e81de7455a6415632bbd32143f0cb
neoduck0 <219318428+neoduck0@users.noreply.github.com>
2026-09-10 19:56:13 +0800

    hypr: fullscreen terminal in special workspace

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -159,7 +159,7 @@ local mainMod = "SUPER"
 
 hl.bind(mainMod .. " + delete", hl.dsp.exec_cmd("hyprlock"), { dont_inhibit = true })
 
-hl.bind(mainMod .. " + return", hl.dsp.exec_cmd(terminal .. " " .. multiplexer), { dont_inhibit = true })
+hl.bind(mainMod .. " + return", hl.dsp.workspace.toggle_special("terminal"), { dont_inhibit = true })
 hl.bind(mainMod .. " + SHIFT + return", hl.dsp.exec_cmd(terminal), { dont_inhibit = true })
 hl.bind(mainMod .. " + period", hl.dsp.exec_cmd(fileManager), { dont_inhibit = true })
 hl.bind(mainMod .. " + slash", hl.dsp.exec_cmd(browser), { dont_inhibit = true })
@@ -319,6 +319,19 @@ hl.workspace_rule({ workspace = "4", monitor = "eDP-1" })
 hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1" })
 
 hl.workspace_rule({ workspace = "special:magic", gaps_in = 20, gaps_out = 40 })
+hl.workspace_rule({
+	workspace = "special:terminal",
+	on_created_empty = terminal .. " " .. multiplexer,
+})
+
+hl.window_rule({
+	name = "fullscreen-terminal",
+	match = {
+		workspace = "special:terminal",
+		class = terminal,
+	},
+	fullscreen = true,
+})
 
 local pin_clients = {
 	"Picture-in-Picture",
